43793b49af730e0101e94270b6de224dccd72691
zhushun0008 <zhushun0008@gmail.com>
2014-09-01 21:52:41 +0800

    Added new file to generate high-resolution sub-images modified:   SR_NE_ANR/CVPR08-SR/Example.m new file:   SR_NE_ANR/CVPR08-SR/generateHighResolutionData.m new file:   SR_NE_ANR/CVPR08-SR/generateTrainingData.m new file:   SR_NE_ANR/CVPR08-SR/lowToHighResoNetMain.m

---
+++ SR_NE_ANR/CVPR08-SR/generateHighResolutionData.m
@@ -0,0 +1,41 @@
+function [subImageHighSet] = generateHighResolutionData(tr_dir, subImageSize, stride)
+% Generate the 'sub-Images' with stride of 14 for training dataset.
+
+fpath = fullfile(tr_dir, '*.bmp');
+img_dir = dir(fpath);
+
+img_num = length(img_dir);
+
+subImageHighSet = {};
+subImageIndex = 0; 
+
+for ii = 1:img_num
+    
+    im = imread(fullfile(tr_dir, img_dir(ii).name));
+    
+% Now I am not sure whether to convert here or do it with python.    
+%     if size(im, 3) == 3
+%         hIm = rgb2ycbcr(im);
+%     else
+%         hIm = im;
+%     end
+    [x, y, z] = size(im);
+    tarSubImageNameFormat = 'Data/highSubImages/subImage%d.bmp';
+    for starty = 1 : stride : y - subImageSize + 1
+        for startx= 1 : stride : x - subImageSize + 1
+            subImageIndex =  subImageIndex + 1;
+            subImage = im(startx : startx + subImageSize - 1, starty : starty + subImageSize - 1, :);
+            subImageName = sprintf(tarSubImageNameFormat, subImageIndex);
+            subImageHighSet = [subImageHighSet,subImage];
+            imwrite(subImage, subImageName,'bmp')
+        end
+    end
+    
+end
+    
+    fprintf('High-resolution sub images are done ...%d\n', size(subImageHighSet));
+ 
+end
+
+    
+    
--- SR_NE_ANR/CVPR08-SR/lowToHighResoNetMain.m
+++ SR_NE_ANR/CVPR08-SR/lowToHighResoNetMain.m
@@ -1,17 +1,14 @@
-% Image super-resolution using sparse representation
-% Example code
+%% =====================================================================
+% Image super-resolution using Deep Convolutional Neural Network
+
+% main code
 %
-% Nov. 2, 2007. Jianchao Yang
-% IFP @ UIUC
-%
-% Revised version. April, 2009.
+% Sep. 01, 2014. Shun Zhu
+
 %
 % Reference
-% Jianchao Yang, John Wright, Thomas Huang and Yi Ma. Image superresolution
-% via sparse representation of raw image patches. IEEE Computer Society
-% Conference on Computer Vision and Pattern Recognition (CVPR), 2008. 
 %
-% For any questions, email me by jyang29@illinois.edu
+% For any questions, email me by zhushun0008@@gmail.com
 
 clear all;
 clc;
@@ -19,39 +16,64 @@ clc;
 addpath('Solver');
 addpath('Sparse coding');
 
+%% ====================================================================
 % =====================================================================
 % specify the parameter settings
 
-patch_size = 3; % patch size for the low resolution input image
-overlap = 1; % overlap between adjacent patches
+patch_size = 32; % patch size for the low resolution input image
+overlap = 14; % Stride/overlap between adjacent patches
+stride = 14; % Stride is big to get nearly independent sub-images
 lambda = 0.1; % sparsity parameter
 zooming = 3; % zooming factor, if you change this, the dictionary needs to be retrained.
 
 tr_dir = 'Data/training'; % path for training images
-skip_smp_training = false; % sample training patches
+skip_generate_HighResolutionData = false; % generating high-resolution sub-images for training
+skip_generate_LowResolutionData = false; % generating low-resolution sub-images for training
+
 skip_dictionary_training = true; % train the coupled dictionary
-num_patch = 50000; % number of patches to sample as the dictionary
+num_patch = 24800; % number of patches to sample as the dictionary
 codebook_size = 1024; % size of the dictionary
 
 regres = 'L1'; % 'L1' or 'L2', use the sparse representation directly, or use the supports for L2 regression
+
+
+%% ====================================================================
 % =====================================================================
-% training coupled dictionaries for super-resolution
+% Generating High-resolution and Low-resolution training data ...
 
-if ~skip_smp_training,
-    disp('Sampling image patches...');
-    [Xh, Xl] = rnd_smp_dictionary(tr_dir, patch_size, zooming, num_patch);
-    save('Data/Dictionary/smp_patches.mat', 'Xh', 'Xl');
-    skip_dictionary_training = false;
-end;
+if ~skip_generate_HighResolutionData
+    disp('Generating High-resolution sub-images ...');
+    highResolutionData = generateHighResolutionData(tr_dir, patch_size, stride);
+    save('Data/highSubImages/highResolutionData.mat');
+    skip_generate_HighResolutionData = true;
+end
 
-if ~skip_dictionary_training,
-    load('Data/Dictionary/smp_patches.mat');
-    [Dh, Dl] = coupled_dic_train(Xh, Xl, codebook_size, lambda);
-    save('Data/Dictionary/Dictionary.mat', 'Dh', 'Dl');
-else
-    load('Data/Dictionary/Dictionary.mat');
-end;
 
+%% Your code here
+% 1. make a directory lowSubImages
+% 2. Implement generateLowResolutionData , you could reference
+% generateHighResolutionData and rnd_smp_dictionary
+
+if ~skip_generate_LowResolutionData
+    disp('Generating Low-resolution sub-images ...');
+    lowResolutionData = generateLowResolutionData(tr_dir, patch_size, zooming, stride);
+    save('Data/lowSubImages/lowResolutionData.mat');
+    skip_generate_LowResolutionData = true;
+end
+
+
+
+
+% 
+% if ~skip_dictionary_training,
+%     load('Data/Dictionary/smp_patches.mat');
+%     [Dh, Dl] = coupled_dic_train(Xh, Xl, codebook_size, lambda);
+%     save('Data/Dictionary/Dictionary.mat', 'Dh', 'Dl');
+% else
+%     load('Data/Dictionary/Dictionary.mat');
+% end;
+
+%% ====================================================================
 % =====================================================================
 % Process the test image 
 
@@ -84,7 +106,9 @@ interpIm2 = rgb2ycbcr(interpIm);
 hImcb = interpIm2(:,:,2);
 hImcr = interpIm2(:,:,3);
 
-% ======================================================================
+
+%% ======================================================================
+% =======================================================================
 % Super-resolution using sparse representation
 
 disp('Start superresolution...');
